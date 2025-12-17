e835c63d00461fc0c8e0a74b9711c302d86bafd0
Omar <95233527+omar94khan@users.noreply.github.com>
2025-12-17 15:24:54 +0300

    Database Defined

---
+++ backend/app/models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, Integer, String, Date, ForeignKey, Float
+from sqlalchemy.orm import relationship
+from database import Base, engine
+
+
+class Users(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    
+
+class Transactions(Base):
+    __tablename__ = "transactions"
+
+    id = Column(Integer, primary_key=True)
+    transaction_user = relationship("Users")
+    user_id = Column(Integer, ForeignKey("users.id"))
+    amount = Column(Float, nullable=False)
+    transaction_type = Column(String, nullable = False)
+    category = Column(String)
+    date = Column(Date, nullable = False)
+    description = Column(String)
+
+Base.metadata.create_all(engine)
+    
