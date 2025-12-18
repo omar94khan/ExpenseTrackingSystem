63a551a7ce09c0d6a8c7ccf3ad426a5212afcb52
Omar <95233527+omar94khan@users.noreply.github.com>
2025-12-19 00:10:15 +0300

    Syntax fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -31,25 +31,27 @@ def create_user(
     return crud.create_user(db, user, hashed_password)
 
 
-@app.get('/users/usercode={id}', response_model = schemas.UserOut)
+@app.get('/users/{id}', response_model = schemas.UserOut)
 def get_user_by_code(
-    user: schemas.UserGetByCode,
+    id: int,
     db: Session = Depends(get_db)
 ):
-    db_user = crud.get_user(db, user.usercode)
+    db_user = crud.get_user(db, id)
     
     if not db_user:
-        raise HTTPException(status_code=400, detail="User doesn't exists")
-    return crud.get_user(db, user)
+        raise HTTPException(status_code=404, detail="User doesn't exists")
+    
+    return db_user
 
 
-@app.get('/users/username={username}', response_model = schemas.UserOut)
+@app.get('/users/by-username/{username}', response_model = schemas.UserOut)
 def get_user_by_username(
-    user: schemas.UserGetByUsername,
+    username: str,
     db: Session = Depends(get_db)
 ):
-    db_user = crud.get_user_by_username(db, user.username)
+    db_user = crud.get_user_by_username(db, username)
     
     if not db_user:
-        raise HTTPException(status_code=400, detail="User doesn't exists")
-    return crud.get_user(db, user)
+        raise HTTPException(status_code=404, detail="User doesn't exists")
+    
+    return db_user
