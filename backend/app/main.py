a6880a73e2427337ffdf6083e9d325ee23fc1bab
Omar <95233527+omar94khan@users.noreply.github.com>
2025-12-20 22:45:38 +0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI
 from .database import engine, Base
 from app.routes import auth, transactions, users
-
+from .routes import auth, transactions, users
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
 
 app.include_router(auth.router)
 app.include_router(users.router)
