015497a52738bd6c62e560b4f12aaa29705c277e
Omar <95233527+omar94khan@users.noreply.github.com>
2026-06-09 22:28:46 +0300

    change backref to back_populate to allow cascaded deletes

--- backend/app/models.py
+++ backend/app/models.py
@@ -10,19 +10,22 @@ class Users(Base):
     username = Column(String, nullable=False)
     hashed_password = Column(String, nullable=False)
     created_on = Column(Date, nullable=True)
+
+    transactions = relationship("Transactions", back_populates="transaction_user", cascade = "all, delete-orphan")
     
 
 class Transactions(Base):
     __tablename__ = "transactions"
 
     id = Column(Integer, primary_key=True)
-    transaction_user = relationship("Users", backref="transactions")
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     amount = Column(Float, nullable=False)
     transaction_type = Column(String, nullable = False)
     category = Column(String)
     date = Column(Date, nullable = False)
     description = Column(String)
+
+    transaction_user = relationship("Users", back_populates="transactions")
 
     
  
