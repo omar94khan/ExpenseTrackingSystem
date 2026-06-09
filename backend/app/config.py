2f74ef0239d4f562189afde5c6b1925eccc71f80
Omar <95233527+omar94khan@users.noreply.github.com>
2026-06-09 22:40:35 +0300

    final fix for backend

--- backend/app/config.py
+++ backend/app/config.py
@@ -8,7 +8,7 @@ def _split_csv(value: str | None) -> List[str]:
 
 ENV = os.getenv("ENV", "local") # change to production in prod environment
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./transactions.db")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:////database/transactions.db")
 
 SECRET_KEY = os.getenv("SECRET_KEY", "1d5as61f561g561gdfa4g51g5615f6sda23")  # must be overridden in prod
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
