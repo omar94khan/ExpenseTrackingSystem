7c68a6bf5797c96176ca18d197b187c11dad663a
Omar <omar.ahmed.khan@hotmail.com>
2026-06-18 14:55:02 +0300

    Creating Card Search Page

--- expense-tracker-frontend/src/pages/CardSearch.jsx
+++ expense-tracker-frontend/src/pages/CardSearch.jsx
@@ -1,5 +1,95 @@
+import { useState } from 'react';
+
 function CardSearch({token}) {
-    return <h1>Card Search Page</h1>
+
+    const [banks, setBanks] = useState([]);
+    const[loading, setLoading] = useState(false);
+    const[selectedBank, setSelectedBank] = useState()
+
+    
+
+    async function getBanks() {
+        const endpointBanks = "http://localhost:8000/banks/fetch"
+        setLoading(true);
+        
+        try {
+            const responseBanks = await fetch(endpointBanks,
+                        {
+                            method: "GET",
+                            headers : {
+                                    "Authorization": "Bearer " + token,
+                                    "Content-Type": "application/json"
+                                }
+                        }
+                    );
+                if (!response.ok) {
+                        const errorData = await response.json();
+                        throw new Error(errorData.detail);
+                    }
+
+                const data = await responseBanks.json();
+                setBanks(data);
+        }
+        catch(err) {
+                throw alert("Error fetching banks: "+err);
+            }
+        finally {
+            setLoading(false)
+        }
+    };
+
+
+    const [cards, setCards] = useState({})
+
+    async function getCards(bank_key) {
+        const endpointCards = "http://localhost:8000/cardlist/getList"
+        setLoading(true);
+
+        try{
+            const responseCards = await fetch(endpointCards,
+                {
+                    method: "GET",
+                    headers: {
+                        "Authorization": "Bearer " + token,
+                        "Content-Type" : "application/json"
+                    },
+                    body: JSON.stringify({
+                        "bank_key" : bank_key
+                    })
+                }
+            );
+            if (!response.ok) {
+                const errorData = await response.json();
+                throw new Error(errorData.detail);
+            }
+
+            const cardData =  await responseCards.json();
+            setCards(cardData);
+        }
+        catch(err) {
+            throw alert("Error fetching card info: "+ err );
+        }
+        finally {
+            setLoading(false);
+        }
+    }
+
+    function bankList() {
+        return(banks.map(row => <option value={selectedBank} onSelect={() => setSelectedBank(row.bank_key)}>{row.bank_name}</option>))
+    }
+
+
+    return (<div>
+                <h2>Card Search Page</h2>
+
+                <table>
+                    <thead>
+                        <tr>
+                            <th></th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>)
 }
 
 export default CardSearch;
