c010f2cbfd710893e44cd8efdb003390226f4d0c
Omar <95233527+omar94khan@users.noreply.github.com>
2026-07-10 03:20:08 +0300

    Validations added in OTP FE

--- expense-tracker-frontend/src/pages/EmailVerification.jsx
+++ expense-tracker-frontend/src/pages/EmailVerification.jsx
@@ -11,6 +11,7 @@ function EmailVerification({token}) {
     const [otp, setOTP] = useState();
     const [loading, setLoading] = useState(false);
     const [editMode, setEditMode] = useState();
+    const [failedAttempts, setFailedAttempts] = useState(0);
 
 
     async function userDetails() {
@@ -34,6 +35,11 @@ function EmailVerification({token}) {
 
 
     async function sendOTP() {
+        if (email === user?.email) {
+            throw alert("This email is already verified. Please enter a different email, or go back.");
+            return;
+        }
+
         const endpoint = '/users/otp/create';
         const options = {
             method : "POST",
@@ -50,6 +56,8 @@ function EmailVerification({token}) {
 
             const data = await response.json()
             setOTPSent(true)
+            setFailedAttempts(0);
+            setEditMode(false);
         }
         catch (err) {
             throw alert("An error occured when trying to generate OTP.")
@@ -62,6 +70,11 @@ function EmailVerification({token}) {
 
 
     async function verifyOTP() {
+        if (!otp) {
+            throw alert("Please input OTP first.");
+            return
+        };
+
         const endpoint = '/users/otp/verify';
         const options = {
             method : "POST",
@@ -82,10 +95,20 @@ function EmailVerification({token}) {
             data?.email_verified ? setEmailVerified(true) : setEmailVerified(false)
             userDetails();
             setOTPSent(false);
-            setEditMode(false);
+            setFailedAttempts(0);
         }
         catch (err) {
-            throw alert("An error occured when trying to verify OTP.")
+            setFailedAttempts(failedAttempts + 1)
+            console.log("Failed attempts = "+failedAttempts);
+            if(failedAttempts >= 2) {
+                setFailedAttempts(0);
+                setEditMode(true);
+                setOTPSent(false);
+                setOTP("");
+                throw alert("Too many failed attempts. Please generate another OTP.");  
+                return
+            };
+            throw alert("An error occured when trying to verify OTP."+err)
         }
         finally{
             setLoading(false);
@@ -120,15 +143,15 @@ function EmailVerification({token}) {
                         </tr>
                         <tr>
                             <th>OTP</th>
-                            <td><input type='text' disabled={emailVerified && !otpSent} 
-                                    onChange={(e) => setOTP(e.target.value)}/></td>
+                            <td><input type='text' 
+                                    disabled={emailVerified && !otpSent} 
+                                    onChange={(e) => setOTP(e.target.value)}
+                                    value={otp}/></td>
                             <td>
-                                <button onClick={() => sendOTP()} disabled={otpSent || (emailVerified && !editMode)}
+                                <button onClick={() => sendOTP()} disabled={(otpSent || emailVerified) && !editMode}
                                         className='inlineRowButtons'>Generate OTP</button>
-                                <button onClick={() => {editMode ? setEditMode(false) : setEditMode(true);
-                                                        verifyOTP();
-                                }} 
-                                        disabled={!otpSent}
+                                <button onClick={() => {verifyOTP()}} 
+                                        disabled={!otpSent || failedAttempts >=3}
                                         className='inlineRowButtons'>   Verify OTP</button>
                             </td>
                         </tr>
